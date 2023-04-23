84b4a5a7a76c0d2df73961e89b07560a8dae8ffb
Archit <architrathod77@gmail.com>
2023-04-23 20:31:50 +0530

    Final Changes

--- powerup/src/App.jsx
+++ powerup/src/App.jsx
@@ -1,187 +1,244 @@
 import React, { useState } from "react";
 // Routers
-import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Header and Footer
-import Navbar from './Base/Navbar'
-import Footer from './Base/Footer'
+import Navbar from "./Base/Navbar";
+import Footer from "./Base/Footer";
 
-import LandingPage from './pages/LandingPage'
+import LandingPage from "./pages/LandingPage";
 
 // Login and Signup
-import Signup from './Login/Signup'
-import Signin from './Login/Signin'
-import Auth from './Login/Auth'
+import Signup from "./Login/Signup";
+import Signin from "./Login/Signin";
+import Auth from "./Login/Auth";
 
 // Goals
-import Goals from './Login/Goals'
+import Goals from "./Login/Goals";
 
 // Dashboards
-import Dashboard from './pages/Dashboards/Dashboard'
-import MDashboard from './pages/Dashboards/MDashboard'
+import Dashboard from "./pages/Dashboards/Dashboard";
+import MDashboard from "./pages/Dashboards/MDashboard";
 
 // Chat
-import DietChatBot from './DietChatBot/Chat'
-import HealthChatBot from './HealthChatBot/Chat'
-import ExerciseChatBot from './ExerciseChatBot/Chat'
+import DietChatBot from "./DietChatBot/Chat";
+import HealthChatBot from "./HealthChatBot/Chat";
+import ExerciseChatBot from "./ExerciseChatBot/Chat";
 
 // Trackers
-import CaloriesIntake from './pages/Trackers/CaloriesIntake'
-import CaloriesBurnt from './pages/Trackers/CaloriesBurnt'
-import WeightTracker from './pages/Trackers/WeightTracker'
-import StepsCounter from './pages/Trackers/StepsCounter'
+import CaloriesIntake from "./pages/Trackers/CaloriesIntake";
+import CaloriesBurnt from "./pages/Trackers/CaloriesBurnt";
+import WeightTracker from "./pages/Trackers/WeightTracker";
+import StepsCounter from "./pages/Trackers/StepsCounter";
 
 // Profile
-import Profile from './pages/Profile/Profile'
-import FriendProfile from './pages/Profile/FriendProfile'
+import Profile from "./pages/Profile/Profile";
+import FriendProfile from "./pages/Profile/FriendProfile";
 
 // Store
-import Store from './pages/Store/Store';
-import Product from './pages/Store/Product';
+import Store from "./pages/Store/Store";
+import Product from "./pages/Store/Product";
 
 export default function App() {
-  
-  return (
-  <Router>
-    <Routes>
-      <Route exact path="/auth/getToken" element={<Auth/>}/>
-      
-      <Route exact path="/" element={<div>
-          
-					
-					<LandingPage/>
-          <Footer/>
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/auth/getToken" element={<Auth />} />
 
-				</div>} />
-      <Route exact path="/signin" element={<div>
+                <Route
+                    exact
+                    path="/"
+                    element={
+                        <div>
+                            <LandingPage />
+                            <Footer />
+                        </div>
+                    }
+                />
+                <Route
+                    exact
+                    path="/signin"
+                    element={
+                        <div>
+                            <Signin />
+                        </div>
+                    }
+                />
+                <Route
+                    exact
+                    path="/signup"
+                    element={
+                        <div>
+                            <Signup />
+                        </div>
+                    }
+                />
+                <Route
+                    exact
+                    path="/goals"
+                    element={
+                        <div>
+                            <Goals />
+                        </div>
+                    }
+                />
 
-					
-					<Signin/>
-          
+                <Route
+                    exact
+                    path="/chat/diet"
+                    element={
+                        <div>
+                            <Navbar />
+                            <DietChatBot />
+                            <Footer />
+                        </div>
+                    }
+                />
 
-				</div>} />
-      <Route exact path="/signup" element={<div>
+                <Route
+                    exact
+                    path="/chat/health"
+                    element={
+                        <div>
+                            <Navbar />
+                            <HealthChatBot />
+                            <Footer />
+                        </div>
+                    }
+                />
 
-					
-					<Signup/>
-          
+                <Route
+                    exact
+                    path="/chat/exercise"
+                    element={
+                        <div>
+                            <Navbar />
+                            <ExerciseChatBot />
+                            <Footer />
+                        </div>
+                    }
+                />
 
-				</div>} />
-      <Route exact path="/goals" element={<div>
+                <Route
+                    exact
+                    path="/profile"
+                    element={
+                        <div>
+                            <Navbar />
+                            <Profile />
+                            <Footer />
+                        </div>
+                    }
+                />
 
-					
-					<Goals/>
-          
+                <Route
+                    exact
+                    path="/dashboard"
+                    element={
+                        <div>
+                            <Navbar />
+                            <Dashboard />
+                            <Footer />
+                        </div>
+                    }
+                />
 
-				</div>} />
+                <Route
+                    exact
+                    path="/m/dashboard"
+                    element={
+                        <div>
+                            <Navbar />
+                            <MDashboard />
+                            <Footer />
+                        </div>
+                    }
+                />
 
-      <Route exact path="/chat/diet" element={<div>
+                <Route
+                    exact
+                    path="/trackers/steps"
+                    element={
+                        <div>
+                            <Navbar />
+                            <StepsCounter />
+                            <Footer />
+                        </div>
+                    }
+                />
 
-					<Navbar/>
-					<DietChatBot/>
-          <Footer/>
+                <Route
+                    exact
+                    path="/trackers/weight"
+                    element={
+                        <div>
+                            <Navbar />
+                            <WeightTracker />
+                            <Footer />
+                        </div>
+                    }
+                />
 
-				</div>} />
+                <Route
+                    exact
+                    path="/trackers/calories-intake"
+                    element={
+                        <div>
+                            <Navbar />
+                            <CaloriesIntake />
+                            <Footer />
+                        </div>
+                    }
+                />
 
-      <Route exact path="/chat/health" element={<div>
+                <Route
+                    exact
+                    path="/trackers/calories-burnt"
+                    element={
+                        <div>
+                            <Navbar />
+                            <CaloriesBurnt />
+                            <Footer />
+                        </div>
+                    }
+                />
 
-					<Navbar/>
-					<HealthChatBot/>
-          <Footer/>
+                <Route
+                    exact
+                    path="/store"
+                    element={
+                        <div>
+                            <Navbar />
+                            <Store />
+                            <Footer />
+                        </div>
+                    }
+                />
 
-				</div>} />
+                <Route
+                    exact
+                    path="/store/:id"
+                    element={
+                        <div>
+                            <Navbar />
+                            <Product />
+                            <Footer />
+                        </div>
+                    }
+                />
 
-      <Route exact path="/chat/exercise" element={<div>
-
-					<Navbar/>
-					<ExerciseChatBot/>
-          <Footer/>
-
-				</div>} />
-      
-<Route exact path="/profile" element={<div>
-
-					<Navbar/>
-					<Profile/>
-          <Footer/>
-
-				</div>} />
-      
-      <Route exact path="/dashboard" element={<div>
-          
-					<Navbar/>
-          <Dashboard />
-          <Footer/>
-
-				</div>} />
-
-      <Route exact path="/m/dashboard" element={<div>
-
-					<Navbar/>
-          <MDashboard />
-          <Footer/>
-
-				</div>} />
-
-      <Route exact path="/trackers/steps" element={<div>
-
-					<Navbar/>
-          <StepsCounter/>
-          <Footer/>
-
-				</div>} />
-
-      <Route exact path="/trackers/weight" element={<div>
-
-					<Navbar/>
-          <WeightTracker />
-          <Footer/>
-
-				</div>} />
-
-      <Route exact path="/trackers/calories-intake" element={<div>
-          
-					<Navbar/>
-          <CaloriesIntake />
-          <Footer/>
-
-				</div>} />
-
-      <Route exact path="/trackers/calories-burnt" element={<div>
-          
-					<Navbar/>
-          <CaloriesBurnt />
-          <Footer/>
-
-				</div>} />
-
-      <Route exact path="/store" element={<div>
-          
-					<Navbar/>
-          <Store />
-          <Footer/>
-
-				</div>} />
-
-      <Route exact path="/store/:id" element={<div>
-          
-					<Navbar/>
-          <Product />
-          <Footer/>
-
-				</div>} />
-
-      <Route exact path="/users/:id" element={<div>
-          
-					<Navbar/>
-          <FriendProfile />
-          <Footer/>
-
-				</div>} />
-
-      
-      
-    </Routes>
-  </Router>
-  )
+                <Route
+                    exact
+                    path="/users/:id"
+                    element={
+                        <div>
+                            <Navbar />
+                            <FriendProfile />
+                            <Footer />
+                        </div>
+                    }
+                />
+            </Routes>
+        </Router>
+    );
 }
--- powerup/src/HealthChatBot/Config.jsx
+++ powerup/src/HealthChatBot/Config.jsx
@@ -1,11 +1,14 @@
 import { createChatBotMessage } from "react-chatbot-kit";
-import { AiFillRobot } from 'react-icons/ai';
+import { AiFillRobot } from "react-icons/ai";
 
 const config = {
-  initialMessages: [createChatBotMessage(`Hello, I am HealthBot! I can give you health advice based on your symptoms. What symptoms are you experiencing?`)],
-  botName: "HealthBot",
-  botAvatar: <AiFillRobot/>,
-  
-}
+    initialMessages: [
+        createChatBotMessage(
+            `Hello, I am HealthBot! I can give you health advice based on your symptoms. What symptoms are you experiencing?`
+        ),
+    ],
+    botName: "HealthBot",
+    botAvatar: <AiFillRobot />,
+};
 
-export default config
+export default config;
